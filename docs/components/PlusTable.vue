6ac726632584361b072f4c2b7aa6679722c65a19
edison <movelovemovelove@163.com>
2025-03-17 18:26:30 +0800

    '暂存'

---
+++ docs/components/PlusTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-table border></el-table>
+  <el-button>你好</el-button>
+</template>
+
+<script setup>
+import 'element-plus/dist/index.css'
+import { ref } from 'vue'
+import { ElTable, ElButton } from 'element-plus'
+</script>
+
+<style>
+
+</style>
---
+++ docs/jbs-components.md
@@ -0,0 +1,12 @@
+---
+outline: deep
+title: JBS Components
+---
+
+**组件示例及组件源代码复制开发中......**
+
+:::demo 这是描述
+
+PlusTable
+
+:::
