a60e0b9c02cc800a7312b2130c60b4931aba9dd9
edison <movelovemovelove@163.com>
2025-03-24 15:42:12 +0800

    暂存辅助函数， 联调接口

--- docs/.vitepress/vitepress/config/helper/sidebar.js
+++ docs/.vitepress/vitepress/config/helper/sidebar.js
@@ -1,5 +1,12 @@
-// sidebar侧边栏的生成函数
-const fs = require('fs');
+/**
+ * sidebar侧边栏的辅助生成函数
+ * 1. 使用目录约束的方法来实现辅助
+ * 2. 所有需要生成sidebar的目录必须遵循： baseFolder/oneLevel/twoLevel...
+ * 3.
+ *
+ * @author kakarotto <movelovemovelove@163.com>
+ */
+const fs = require('node:fs');
 function generateSidebar(){
 
   return 
